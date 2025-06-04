1f0e3f5dfe006f733bd2066f57dd70e3ead5264e
Hiral jain <jjhiral2005@gmail.com>
2025-06-04 23:00:26 +0530

    fix: added vercel-build script

--- PROJECT/Backend/Supabase/.env.d.ts
+++ PROJECT/Backend/Supabase/.env.d.ts
@@ -2,4 +2,4 @@
 // env.d.ts
 export const SUPABASE_URL = "https://siayecqdxwvgagmpkcmq.supabase.co";
 export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpYXllY3FkeHd2Z2FnbXBrY21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1ODg5NjQsImV4cCI6MjA2NDE2NDk2NH0.WTzCTGgCiZdtTbHUgH8IoqyizpSi2OIor_nsinDzomU";
-REACT_APP_API_BASE_URL="https://32d9-49-43-224-133.ngrok-free.app"
+REACT_APP_API_BASE_URL="https://c91d-49-43-224-133.ngrok-free.app"
--- PROJECT/Frontend/.next/static/chunks/webpack.js
+++ PROJECT/Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c93d184e84dadfe9"; }
+/******/ 		__webpack_require__.h = function() { return "c856a60c77df010f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
